6ff7fc554d1cbf3e726feca4a101b6d3db57bcd8
Simon1675 <61452397+Simon1675@users.noreply.github.com>
2021-12-08 15:01:39 +0100

    BigCommit

--- frontend/src/components/API.js
+++ frontend/src/components/API.js
@@ -1,35 +1,75 @@
 import React from 'react';
 import { useEffect , useState} from 'react';
 import axios from 'axios';
-import '../styles/Style.css';
 import Api from '../styles/Api.css'
 
+class API extends React.Component {
 
-const API = () => {
-        const [state , setState ] = useState ({
-            joke :''
+    state = {
+        title: '',
+        body: ''
+    };
+
+handleChange = (event) => {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+
+        this.setState({
+            [name]: value
         });
+    };
 
-        useEffect( ()  => {
-            fetchData();
-        }, []);
-
-        const fetchData = async () => {
-        const result= await axios.get("http://localhost:8000/api");
-        console.log(result.data);
-        setState({
-            ...state,
-            joke : result.data
+submit = (event) => {
+        event.preventDefault();
+        const payload = {
+            title: this.state.title,
+            body: this.state.body
+        };
+        axios({
+        url: '/api/save',
+        method: 'FORM',
+        data: payload
+    })
+        .then(() => {
+            console.log('Les données ont été envoyé vers le server')
         })
+        .catch(() => {
+            console.log('Erreur')
+        });;
+    };
 
-    }
+    render() {
 
+        console.log('State: ', this.state);
 
-    return (
-        <div className="API">
-            <h1 className="API_titre">Form</h1>
+    return(
+        <div>
+            <h2 className="API_titre">Give us some ideas</h2>
+            <form className="form"> 
+                <div className="form-input1"></div>
+                    <input 
+                    type="text" 
+                    name="title"
+                    placeholder="Title"
+                    value={this.state.title}
+                    onChange={this.handleChange}
+                    />
+                <div className="form-input2">
+                    <textarea 
+                    placeholder="body"
+                    name="body" 
+                    id="" 
+                    cols="30" 
+                    rows="10" 
+                    value={this.state.body}
+                    onChange={this.handleChange}></textarea>
+                </div>
+                <button className="btn_submit">Submit</button>
+            </form>
         </div>
     );
-};
+}
+}
 
 export default API;
